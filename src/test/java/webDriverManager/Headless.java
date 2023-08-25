4412772f5ed7b6036ce0bfbaceafa3833c433525
Permyakov Vitalij <permvpva@yandex.ru>
2023-08-25 21:37:19 +0300

    Доработка

--- src/test/java/webDriverManager/Headless.java
+++ src/test/java/webDriverManager/Headless.java
@@ -10,26 +10,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.concurrent.TimeUnit;
 
 public class Headless {
 
-  //TODO !!! ТРЕБУЕТСЯ ВЕРСИЯ Google chrome 114 !!!
-
   private WebDriver driver;
-  private void enterTextArea(WebElement element, String text){
-    element.clear();
-    element.sendKeys(text);
-  }
+
   @BeforeEach
   public void before() {
     WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--remote-allow-origins=*");
-//    options.addArguments("headless");
     options.addArguments("--headless");
-//    options.addArguments("--start-fullscreen");
     driver = new ChromeDriver(options);
+
   }
 
   @AfterEach
@@ -44,9 +39,11 @@ public class Headless {
   public void testOne() {
     driver.get("https://duckduckgo.com/");
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.findElement(By.cssSelector("input[id='search_form_input_homepage']")).sendKeys("ОТУС");
-    driver.findElement(By.cssSelector("#search_button_homepage")).click();
+    driver.findElement(By.cssSelector("input[name='q'")).sendKeys("ОТУС");
+    driver.findElement(By.cssSelector("input[name='q'")).submit();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...", driver.findElement(By.cssSelector("#r1-0 a[data-testid='result-title-a'] span")).getText());
+    Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...",
+        driver.findElement(By.cssSelector("#r1-0 a[data-testid='result-title-a'] span")).getText());
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   }
 }
