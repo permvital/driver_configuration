1567a2d9b8ce2b62fb2efc3e53b8fcf0d340487f
permvital <permvpva@yandex.ru>
2024-02-04 21:48:04 +0300

    Доработка

--- src/test/java/webDriverManager/Fullscreen.java
+++ src/test/java/webDriverManager/Fullscreen.java
@@ -10,13 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 import java.util.Set;
 
 public class Fullscreen {
@@ -27,7 +22,7 @@ public class Fullscreen {
   private final String PASSWORD = "fdgkj8w9Ds!";
 
   @BeforeAll
-  public static void beforeAll(){
+  public static void beforeAll() {
     WebDriverManager.chromedriver().setup();
   }
 
@@ -48,35 +43,19 @@ public class Fullscreen {
     }
   }
 
-  @Test
-  public void testThree() {
-    driver.get("https://otus.ru");
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    driver.manage().deleteAllCookies();
-    driver.findElement(new By.ByXPath("//button[text()='Войти']")).click();
-    WebElement loginInput = driver.findElement(new By.ByXPath("//input[@name='email']"));
-    loginInput.clear();
-    loginInput.sendKeys(LOGIN);
-    WebElement passInput = driver.findElement(new By.ByXPath("//input[@type='password']"));
-    passInput.clear();
-    passInput.sendKeys(PASSWORD);
-    driver.findElement(new By.ByXPath("//button/div[text()='Войти']")).click();
-    logger.info(String.format("Выводим в логи Cookies с otus.ru %s", driver.manage().getCookies().toString()));
+      @Test
+      public void testThree() {
+        driver.get("https://otus.ru");
+        driver.findElement(By.cssSelector(".iOoJwQ")).click();
+        driver.findElement(By.xpath("//*[@id=\"__PORTAL__\"]/div/div/div[3]/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div")).click();
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(LOGIN);
+        driver.findElement(By.xpath("//*[@id=\"__PORTAL__\"]/div/div/div[3]/div[2]/div/div[2]/div[1]/div/div[1]/div/div[2]/div")).click();
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(PASSWORD);
+        driver.findElement(By.cssSelector("button[type=button] div.sc-9a4spb-2"));
+        Set<Cookie> cookie = driver.manage().getCookies();
+        for (Cookie cookies : cookie) {
+          logger.info("Name = " + cookies.getName() + ", " + "Value = " + cookies.getValue());
+
+    }
   }
 }
-
-//      @Test
-//      public void testThree() {
-//        driver.get("https://otus.ru");
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        driver.findElement(By.cssSelector(".iOoJwQ")).click();
-//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='email']"))).sendKeys(LOGIN);
-////    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='text'][@name='email']"))).sendKeys(LOGIN);
-//        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(PASSWORD);
-//        driver.findElement(By.cssSelector("button[type=button] div.sc-9a4spb-2"));
-//        Set<Cookie> cookie = driver.manage().getCookies();
-//        for (Cookie cookies : cookie) {
-//          logger.info("Name = " + cookies.getName() + ", " + "Value = " + cookies.getValue());
-//    }
-//  }
-//}
